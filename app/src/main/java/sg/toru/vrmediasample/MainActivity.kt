4cf6ad0b448eae39ecad642f72fb61a86aaff68a
Toru Choi <blueocean239@gmail.com>
2019-09-24 15:21:35 +0800

    ExoPlayer configuration

--- app/src/main/java/sg/toru/vrmediasample/MainActivity.kt
+++ app/src/main/java/sg/toru/vrmediasample/MainActivity.kt
@@ -2,11 +2,12 @@ package sg.toru.vrmediasample
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.google.android.exoplayer2.ExoPlayerFactory
 import com.google.android.exoplayer2.ui.PlayerView
-import com.google.android.exoplayer2.SimpleExoPlayer
 import com.google.android.exoplayer2.source.MediaSource
 import android.net.Uri
+import android.os.Build
+import android.util.Log
+import com.google.android.exoplayer2.*
 import com.google.android.exoplayer2.upstream.DefaultHttpDataSourceFactory
 import com.google.android.exoplayer2.source.ExtractorMediaSource
 
@@ -14,22 +15,97 @@ class MainActivity : AppCompatActivity() {
 
     private lateinit var videoPlayer:PlayerView
     private lateinit var exoPlayerInstance:SimpleExoPlayer
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         videoPlayer = findViewById(R.id.exoplayer)
         exoPlayerInstance = ExoPlayerFactory.newSimpleInstance(this)
         videoPlayer.player = exoPlayerInstance
+        videoPlayer.player.addListener(object: Player.EventListener{
+            override fun onPlayerStateChanged(playWhenReady: Boolean, playbackState: Int) {
+                when(playbackState){
+                    Player.STATE_IDLE->{
+                        Log.i("TORU", "STATE IDLE")
+                    }
+                    Player.STATE_BUFFERING->{
+                        isPlaying = false
+                        Log.i("TORU", "STATE BUFFERING")
+                    }
+                    Player.STATE_ENDED->{
+                        isPlaying = false
+                        Log.i("TORU", "STATE ENDED")
+                    }
+                    Player.STATE_READY->{
+                        isPlaying = true
+                        Log.i("TORU", "STATE READY")
+                    }
+                }
+            }
+
+            override fun onPlayerError(error: ExoPlaybackException?) {
+                error?.printStackTrace()
+            }
+        })
 
         val uri = Uri.parse(url)
         val mediaSource = buildMediaSource(uri)
         exoPlayerInstance.prepare(mediaSource, true, false)
+        videoPlayer.player.playWhenReady = true
+        videoPlayer.player.seekTo(0)
     }
 
     private fun buildMediaSource(uri: Uri): MediaSource {
         return ExtractorMediaSource.Factory(
-            DefaultHttpDataSourceFactory("exoplayer-codelab")
+            DefaultHttpDataSourceFactory("vs-singapore")
         ).createMediaSource(uri)
+    }
+
+    private var isPlaying = false
+
+    override fun onStart() {
+        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.M){
+            videoPlayer.onResume()
+            if(isPlaying){
+                val uri = Uri.parse(url)
+                val mediaSource = buildMediaSource(uri)
+                exoPlayerInstance.prepare(mediaSource, !isPlaying, false)
+            }
+        }
+        super.onStart()
+    }
+
+    override fun onResume() {
+        if(Build.VERSION.SDK_INT < Build.VERSION_CODES.M){
+            videoPlayer.onResume()
+            if(isPlaying){
+                val uri = Uri.parse(url)
+                val mediaSource = buildMediaSource(uri)
+                exoPlayerInstance.prepare(mediaSource, !isPlaying, false)
+            }
+        }
+        super.onResume()
+    }
+
+    override fun onPause() {
+        super.onPause()
+        if(Build.VERSION.SDK_INT < Build.VERSION_CODES.M){
+            videoPlayer.player.stop(false)
+            videoPlayer.onPause()
+        }
+    }
+
+    override fun onStop() {
+        super.onStop()
+        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.M){
+            videoPlayer.player.stop(false)
+            videoPlayer.onPause()
+        }
+    }
+
+    override fun onDestroy() {
+        videoPlayer.player.release()
+        super.onDestroy()
     }
 
     companion object{
